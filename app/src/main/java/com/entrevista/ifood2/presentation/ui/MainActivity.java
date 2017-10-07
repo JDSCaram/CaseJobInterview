3bcded9ad6e09d5bbf04b0f3c8d8ec6e5572f980
JDSCaram <jonatas.caram@hotmail.com>
2017-10-07 12:48:25 -0300

    Desenvolvido o fluxo de Telas basico da aplicação  Fragment Lista > Menu > Detalhes

--- app/src/main/java/com/entrevista/ifood2/presentation/ui/MainActivity.java
+++ app/src/main/java/com/entrevista/ifood2/presentation/ui/MainActivity.java
@@ -1,18 +1,57 @@
 package com.entrevista.ifood2.presentation.ui;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.entrevista.ifood2.R;
+import com.entrevista.ifood2.presentation.ui.cart.CartActivity;
+import com.entrevista.ifood2.presentation.ui.restaurants.RestaurantFragment;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        attachFragment(new RestaurantFragment(), RestaurantFragment.class.getSimpleName());
+    }
+
+    public void attachFragment(Fragment fragment, String tag) {
+        manager = getSupportFragmentManager();
+        manager.findFragmentByTag(tag);
+        FragmentTransaction ft = manager.beginTransaction();
+
+        if (manager.findFragmentByTag(tag) == null) { // Sem fragment no backstack com mesmo nome.
+            ft.add(R.id.frame_content, fragment, tag);
+            ft.addToBackStack(tag);
+            ft.commit();
+        } else {
+            ft.show(manager.findFragmentByTag(tag)).commit();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(manager.getBackStackEntryCount() <= 1 ) {
+            finish();
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -21,5 +60,18 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.ifood_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent i = new Intent(MainActivity.this, CartActivity.class);
+                startActivity(i);
+                return false;
+            }
+        });
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/entrevista/ifood2/presentation/ui/restaurants/RestaurantFragment.java
+++ app/src/main/java/com/entrevista/ifood2/presentation/ui/restaurants/RestaurantFragment.java
@@ -1,10 +1,41 @@
 package com.entrevista.ifood2.presentation.ui.restaurants;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.entrevista.ifood2.R;
+import com.entrevista.ifood2.presentation.ui.MainActivity;
+import com.entrevista.ifood2.presentation.ui.menu.MenuFragment;
 
 /**
  * Created by JCARAM on 05/10/2017.
  */
 
 public class RestaurantFragment extends Fragment {
+
+    private TextView mTextView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_restaurant, container, false);
+        mTextView = (TextView) view.findViewById(R.id.text_view);
+        afterViews();
+        return view;
+    }
+
+    private void afterViews() {
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)getActivity()).attachFragment(new MenuFragment(),
+                        MenuFragment.class.getSimpleName());
+            }
+        });
+    }
 }
